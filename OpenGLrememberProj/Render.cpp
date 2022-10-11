59db38595ab08c73cfbfbc298ac802e5576a7939
Atoll <renovatedatol@inbox.ru>
2022-10-11 21:13:33 +0300

    initial

---
+++ OpenGLrememberProj/Render.cpp
@@ -0,0 +1,130 @@
+
+
+#include "Render.h"
+
+#include <Windows.h>
+#include <GL\GL.h>
+#include <GL\GLU.h>
+
+class draw
+{
+public:
+	//Ќачало с конца (в противном случае фиугра рисуетс€ по часовой, а не против)
+	/*
+	double dot_1[3] = { 0, 1, height };
+	double dot_2[3] = { 5, 4, height };
+	double dot_3[3] = { 5, 0, height };
+	double dot_4[3] = { 1, -1, height };
+	double dot_5[3] = { 1, -6, height };
+	double dot_6[3] = { -1, -2, height };
+	double dot_7[3] = { -7, -2, height };
+	double dot_8[3] = { -6, 5, height };
+	double dot_9[3] = { 0, 1, height };
+	*/
+
+	void draw_the_thing(double height)
+	{
+		glBegin(GL_POLYGON);
+
+		glVertex3d(0, 1, height);
+		glVertex3d(-6, 5, height);
+		glVertex3d(-7, -2, height);
+		glVertex3d(-1, -2, height);
+		glVertex3d(1, -6, height);
+		glVertex3d(1, -1, height);
+		glVertex3d(5, 0, height);
+		glVertex3d(5, 4, height);
+		glVertex3d(0, 1, height);
+
+		glEnd();
+	}
+
+	void connect_sides(double height_bottom, double height_upper)
+	{
+		glBegin(GL_QUADS);
+
+		glVertex3d(0, 1, height_bottom);
+		glVertex3d(-6, 5, height_bottom);
+		glVertex3d(-6, 5, height_upper);
+		glVertex3d(0, 1, height_upper);
+
+		//впукла€ часть
+		glColor3d(1, 0, 1);
+		glVertex3d(-6, 5, height_bottom);
+		glVertex3d(-7, -2, height_bottom);
+		glVertex3d(-7, -2, height_upper);
+		glVertex3d(-6, 5, height_upper);
+
+		glColor3d(0, 0, 0);
+		glVertex3d(-7, -2, height_bottom);
+		glVertex3d(-1, -2, height_bottom);
+		glVertex3d(-1, -2, height_upper);
+		glVertex3d(-7, -2, height_upper);
+
+		glVertex3d(-1, -2, height_bottom);
+		glVertex3d(1, -6, height_bottom);
+		glVertex3d(1, -6, height_upper);
+		glVertex3d(-1, -2, height_upper);
+
+		glVertex3d(1, -6, height_bottom);
+		glVertex3d(1, -1, height_bottom);
+		glVertex3d(1, -1, height_upper);
+		glVertex3d(1, -6, height_upper);
+
+
+		glVertex3d(1, -1, height_bottom);
+		glVertex3d(5, 0, height_bottom);
+		glVertex3d(5, 0, height_upper);
+		glVertex3d(1, -1, height_upper);
+
+		/*
+		glBegin(GL_QUADS);
+		int x_1 = 5;
+		int y_1 = 0;
+		int x_2 = 5;
+		int y_2 = 4;
+		//draw d1_b -> d2_b -> d2_u -> d1_u
+		double dot_1_bottom[3] = { x_1, y_1, height_bottom };
+		double dot_1_upper[3] = { x_1, y_1, height_upper };
+		double dot_2_bottom[3] = { x_2, y_2, height_bottom };
+		double dot_2_upper[3] = { x_2, y_2, height_upper };
+
+		double dot_1_bottom_small[3] = { x_1, y_1, height_bottom };
+		double dot_1_upper_small[3] = { x_1, y_1, height_upper };
+		double dot_2_bottom_small[3] = { x_2, y_2, height_bottom };
+		double dot_2_upper_small[3] = { x_2, y_2, height_upper };
+		*/
+
+		//выпукла€ часть
+		glColor3d(0, 1, 0);
+		glVertex3d(5, 0, height_bottom);
+		glVertex3d(5, 4, height_bottom);
+		glVertex3d(5, 4, height_upper);
+		glVertex3d(5, 0, height_upper);
+
+		glColor3d(0, 0, 0);
+		glVertex3d(5, 4, height_bottom);
+		glVertex3d(0, 1, height_bottom);
+		glVertex3d(0, 1, height_upper);
+		glVertex3d(5, 4, height_upper);
+
+		glEnd();
+		
+	}
+
+	void draw_in3d(double height_bottom, double height_upper)
+	{
+		draw_the_thing(height_bottom);
+		draw_the_thing(height_upper);
+		connect_sides(height_bottom, height_upper);
+	}
+};
+
+void Render(double delta_time)
+{    
+	draw thing;
+	thing.draw_in3d(0, 1);
+
+
+}   
+
